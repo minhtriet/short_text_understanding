a9c3bf8a2b02c65f1cdd04499884515ee4a8095d
mt <mtriet.chau@gmail.com>
2020-07-19 00:50:45 +0200

    draft server

---
+++ src_python/server.py
@@ -0,0 +1,24 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from flair.data import Sentence
+from flask import Flask
+app = Flask(__name__)
+
+tagger = SequenceTagger.load('chunk')
+@app.route('/api')
+def api_home():
+    return 42;
+
+@app.route('/nerd_api/v1', method='POST')
+def nerd(query):
+    data = request.get_json(force=True)
+    input_params = data['input']
+
+    sentence = Sentence(input_params['sentence'])
+    print(f'this tagger predicts the {tagger.tag_type} tag')
+    tagger.predict(sentence)
+    print(sentence)
+    print('The following chunk tags are found:')
+    for entity in sentence.get_spans('np'):
+        print(entity)
+
