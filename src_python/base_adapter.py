6930a6ec706524db3fd9b43262d8d695dd918b97
mt <mtriet.chau@gmail.com>
2020-07-23 21:30:36 +0200

    edit base adapter

--- src_python/base_adapter.py
+++ src_python/base_adapter.py
@@ -1,22 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import List
+from typing import Dict
 
 @dataclass
 class Entity:
     name: str
-    probabity: float
+    probability: float
     description: str
     uri: str
 
 
 class EntityDatabase(ABC):
 
-    def __init__(entity):
-        self.entity = entity
+    @abstractmethod
+    def __init__(self, entity):
+        return NotImplemented
 
     @abstractmethod
-    def __possible_entities__(self) -> List[Entity]:
-        raise NotImplemented
+    def get_probabilities(self) -> Dict:
+        return NotImplemented
 
-         
