f8a0772707734b2b0e07f3c513d7f0d2af6d0e5e
minhtriet <mtriet.chau@gmail.com>
2020-07-27 16:57:02 +0200

    remove unnecessary dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { process } from './nlp';
 import './style.css';
 
 
--- src/nlp.js
+++
@@ -1,7 +0,0 @@
-const server_url = "127.0.0.1:5000/api/v1/";
-
-export const process = (query) => {
-  alert("cool");
-//    fetch(server_url)
-//      .then(response => response.json());
-}
