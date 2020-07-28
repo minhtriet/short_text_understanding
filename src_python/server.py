11e72f9cb1ca263c87fe069e53a00e9321633f23
minhtriet <mtriet.chau@gmail.com>
2020-07-28 20:47:50 +0200

    move nerd logic out of flask server

--- src_python/server.py
+++ src_python/server.py
@@ -1,21 +1,8 @@
-from flair.models import SequenceTagger
-from flair.data import Sentence
 from flask import Flask, request, jsonify
-import sys
-import logging
 
-# import nerd
-import wikidata_adapter
-
+import nerd
 app = Flask(__name__)
 
-logger = logging.getLogger("app")
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-logger.addHandler(handler)
-
-tagger = SequenceTagger.load('chunk')
 
 @app.route('/api')
 def api_home():
@@ -24,23 +11,8 @@ def api_home():
 @app.route('/api/v1', methods=['GET'])
 def nerd():
     # chunking
-    sentence = Sentence(request.args.get('query'))
-    tagger.predict(sentence)
-    logging.log(logging.DEBUG, sentence)
-    print('The following chunk tags are found:')
-    entities = []
-    for entity in sentence.get_spans('np'):
-        print(entity)
-        entity_dict = entity.to_dict()
-        entity_dict['tag'] = entity.tag
-        entity_dict['score'] = entity.score
-        # {'text': 'buy', 'start_pos': 0, 'end_pos': 3, 'labels': [VP (0.9305)]}
-        # nerd with wikidata
-        wikidata = wikidata_adapter.WikidataAdapter(entity_dict['text'])
-        probs = wikidata.to_entity_list()
-        if probs:
-            entities.append(probs)
-    logging.log(logging.DEBUG, entities)
+    entities = nerd.disambiguate(request.args.get('query'))
     if entities:
         return jsonify(entities)
     return jsonify({"error": "no entities"})
+    return None
