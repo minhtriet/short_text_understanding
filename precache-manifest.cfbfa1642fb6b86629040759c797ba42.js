ce21d228fa383b22c4080414fff7fe1404f00f54
minhtriet <mtriet.chau@gmail.com>
2020-07-30 23:37:40 +0200

    Updates

---
+++ precache-manifest.cfbfa1642fb6b86629040759c797ba42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013318938e072d63e9c2ff14bb21d7bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f2145a894468671b4e",
+    "url": "/static/css/main.3396f368.chunk.css"
+  },
+  {
+    "revision": "e0a75ca6c04333238a16",
+    "url": "/static/js/2.fa33b71f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa33b71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f2145a894468671b4e",
+    "url": "/static/js/main.b2352855.chunk.js"
+  },
+  {
+    "revision": "65a24d13a7bde0eb0cb4",
+    "url": "/static/js/runtime-main.28b204b3.js"
+  }
+]);
