5b53687c91f50b7e39ea2609facb0a561a365fd2
minhtriet <mtriet.chau@gmail.com>
2020-08-01 00:30:42 +0200

    move from chunking to pos

--- src_python/base_adapter.py
+++ src_python/base_adapter.py
@@ -7,12 +7,13 @@ class Entity:
     name: str
     probability: float
     description: str
-    uri: str
     start_pos: int = 0
     end_pos: int = 0
 
 
 class EntityDatabase(ABC):
+
+    MAX_RESULTS_NUMBER = 4
 
     @abstractmethod
     def __init__(self, entity):
--- src_python/nerd.py
+++ src_python/nerd.py
@@ -18,6 +18,8 @@ import logging
 tagger = SequenceTagger.load('chunk')
 NOUN_PHRASE_TAG = 'NP'
 VERB_PHRASE_TAG = 'VP'
+tagger = SequenceTagger.load('pos')
+
 
 flair_embedding_forward = FlairEmbeddings('news-forward')
 flair_embedding_backward = FlairEmbeddings('news-backward')
@@ -39,9 +41,21 @@ def disambiguate(query) -> List[Entity]:
     for entity in sentence.get_spans('np'):
         if entity.tag == NOUN_PHRASE_TAG:
             print(entity)
-            wikidata = wikidata_adapter.WikidataAdapter(entity.text)
-            possible_entities = wikidata.to_entity_list()
-            if possible_entities:
+            biggest_probs = 0
+            best_config = (0, len(entity))
+            best_entities = None
+            # extract subtext with most claims
+            for start_index in range(1, len(entity) - 1):
+                for end_index in range(start_index+1, len(entity)):
+                    sub_text = ' '.join(list(map(lambda x: x.text, entity.tokens[start_index:end_index+1])))
+                    wikidata = wikidata_adapter.WikidataAdapter(sub_text)
+                    total_prob, possible_entities = wikidata.to_entity_list()
+                    if total_prob > biggest_probs:
+                        biggest_probs = total_prob
+                        best_entities = possible_entities
+                        best_config = (start_index, end_index)
+            # use words around that subtext to gain more data for likelihood
+            if best_entities:
                 similarities = []
                 if effective_verb_embedding:
                     # reduce two arrays of embedding to a number of maximum similarity
@@ -49,7 +63,6 @@ def disambiguate(query) -> List[Entity]:
                         desc = Sentence(prob.description)
                         flair_embedding_forward.embed(desc)
                         similarities.append(max([np.dot(token_prime.embedding, token.embedding) for token in desc for token_prime in effective_verb_embedding]))
-                    import pdb; pdb.set_trace()
                     similarities = special.softmax(similarities)
                     # bayes rule
                     posteriors = similarities * [entity.probability for entity in possible_entities]
@@ -59,9 +72,9 @@ def disambiguate(query) -> List[Entity]:
             possible_entities[most_likely_index].start_pos = entity.start_pos
             possible_entities[most_likely_index].end_pos = entity.end_pos
             most_likely_entities.append(possible_entities[most_likely_index])
-        if entity.tag == VERB_PHRASE_TAG:
-            effective_verb_embedding = Sentence(entity.text)
-            flair_embedding_forward.embed(effective_verb_embedding)
+            if entity.tag == VERB_PHRASE_TAG:
+                effective_verb_embedding = Sentence(entity.text)
+                flair_embedding_forward.embed(effective_verb_embedding)
     logging.log(logging.DEBUG, most_likely_entities)
     return most_likely_entities if most_likely_entities else None
 
