99d8165ea0f047473e1f132b4bbf410f196076b1
minhtriet <mtriet.chau@gmail.com>
2020-08-04 17:36:00 +0200

    bind response with element on dom tree

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import './skeleton.css';
 import './style.css';
 
 
-class TextBoxQuery extends React.Component {
+class QueryForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {query: ''};
+    this.state = {query: '', result: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -17,7 +17,12 @@ class TextBoxQuery extends React.Component {
     fetch(`/api/v1?query=${encodeURIComponent(this.state.query)}`)
     .then(response => response.json())
     .then(
-      (result) => {console.log(result)},
+      (result) => {
+        if ('error' in result) {
+          this.setState({result: "No enitity found in your query"});
+        }
+        console.log(result)
+      },
       (error) => {console.log(error)}
     );
     event.preventDefault();
@@ -33,6 +38,9 @@ class TextBoxQuery extends React.Component {
         <form onSubmit={this.handleSubmit} method="GET">
               <input id='textbox_query' value={this.state.query} onChange={this.handleChange} type='text' autoFocus/>
         </form>
+        <div className="result">
+          {this.state.result}
+        </div>
       </div>
     );
   }
@@ -43,10 +51,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>nerd</h1>
-        <TextBoxQuery />
-        <div className="result">
-          <mark> Jack </mark> is cool
-        </div>
+        <QueryForm />
       </div>
     );
   }
