316e015af4ef0eb7815460138a159f25296bd87a
minhtriet <mtriet.chau@gmail.com>
2020-08-10 18:08:20 +0200

    beautify tests

--- src_python/test_nerd.py
+++ src_python/test_nerd.py
@@ -1,21 +1,19 @@
 import nerd
 import pytest
 
-def test_one_entity():
-    result = nerd.disambiguate("read harry potter")
-    assert len(result) == 1
-    assert result[0].name == 'Q8337'  # Harry Potter the series
-
-def test_one_entity_with_noise():
-    result = nerd.disambiguate("thick harry potter book")
-    assert len(result) == 1
-    assert result[0].name == 'Q3244512'  # Harry Potter the character
-    # Q3244512, interestingly, not Q8337 because thick series book seems nonsense
-
-def test_another_entity():
-    result = nerd.disambiguate("watch harry potter")
-    assert len(result) == 1
-    assert result[0].name == 'Q216930'  # Harry Potter the film
+@pytest.mark.parametrize("test_input,expected_len,expected_entity", 
+    [("and", 0, None),
+      ("read harry potter", 1, "Q8837"),
+      ('thick harry potter book', 1, 'Q3244512'),   # Q3244512, not Q8337, because thick series book seems nonsense
+      ("watch harry potter", 1, "Q216930"),
+      ('rage against the machine', 1, 'Q72092'),])
+def test_one_entity(test_input, expected_len, expected_entity):
+    result = nerd.disambiguate(test_input)
+    if not expected_entity:
+        assert 'error' in result[0].keys
+    else:
+        assert len(result) == expected_len
+        assert result[0]['name'] == expected_entity
 
 def test_consistency():
     result_0 = nerd.disambiguate("read harry potter")
@@ -24,8 +22,4 @@ def test_consistency():
     assert len(result) == 2
     assert result[0].name == result_0[0].name and result[1].name == result_1[0].name
 
-
-def test_long_entity()
-    result = nerd.disambiguate('rage against the machine');
-    assert result[0].name == 'Q72092'
 
