115208de3aa2925f5e0e986fe056afe96ad0b90e
minhtriet <mtriet.chau@gmail.com>
2020-08-10 18:07:33 +0200

    add v2 for WikidataAdapter

--- src_python/nerd.py
+++ src_python/nerd.py
@@ -107,3 +107,73 @@ def disambiguate(query) -> List[Entity]:
     logging.log(logging.DEBUG, most_likely_entities)
     return most_likely_entities if most_likely_entities else None
 
+
+def disambiguate_v2(query) -> List[Entity]:
+    sentence = Sentence(query)
+    tagger.predict(sentence)
+    logging.log(logging.DEBUG, sentence)
+    most_likely_entities = []
+    trailing = 0
+    token_index = 0
+    total_prob, possible_entities = 0, None
+    while token_index < len(sentence.tokens):
+        # todo: change this to binary search, maybe to v2
+        token = sentence[token_index]
+        if token.get_tag('pos').value in [NOUN_TAG, PROPN_TAG]:
+            if token_index == len(sentence) - 1:
+              longest_token_index = len(sentence)
+            else:
+              for longest_token_index in range(token_index + 1, len(sentence)):
+                  if sentence[longest_token_index].get_tag('pos').value in [NOUN_TAG, PROPN_TAG]:
+                      break
+            biggest_probs = float('-inf')
+            best_config = (token_index, token_index)
+            possible_entities = None
+            # extract subtext with most claims in the database
+            for start_index in range(token_index, longest_token_index):
+                for end_index in range(start_index, longest_token_index + 1):
+                    sub_text = ' '.join(list(map(lambda x: x.text, sentence[start_index:end_index+1])))  # end_index included
+                    wikidata = wikidata_adapter.WikidataAdapter(sub_text)
+                    total_prob, temp_possible_entities = wikidata.to_entity_list()
+                    if temp_possible_entities and temp_possible_entities[0].probability > biggest_probs:   # only consider most relevant entity
+                        biggest_probs = temp_possible_entities[0].probability
+                        best_config = (start_index, end_index)
+                        possible_entities = temp_possible_entities
+            # found an entity!!!
+            # now what are the trailing tokens?
+            # the var `trailing` is from previous entity!
+            preceding_part = Sentence(' '.join(list(map(lambda x: x.text, sentence[trailing:best_config[0]]))))
+            flair_embedding_forward.embed(preceding_part)
+            # now updating `trailing`
+            for trailing in range(best_config[1], len(sentence)):
+                if sentence[trailing].get_tag('pos').value in [ADJ_TAG, VERB_TAG]:
+                    break
+            token_index = trailing + 1
+            succeeding_part = Sentence(' '.join(list(map(lambda x: x.text, sentence[best_config[1]+1:trailing+1]))))
+            flair_embedding_backward.embed(succeeding_part)
+            # use words around that subtext to gain more data for likelihood
+            if possible_entities:
+                # get possible entities
+                similarities = []
+                if preceding_part or succeeding_part:
+                    # reduce two arrays of embedding to a number of maximum similarity
+                    for prob in possible_entities:
+                        desc = Sentence(prob.description)
+                        flair_embedding_forward.embed(desc)
+                        best_sim = max(sentence_similarity(desc, preceding_part), sentence_similarity(desc, succeeding_part))
+                        similarities.append(best_sim)
+                    similarities = special.softmax(similarities)
+                    # bayes rule
+                    # posteriors = similarities * [entity.probability for entity in possible_entities]
+                    # most_likely_index = np.argmax(posteriors)
+                    most_likely_index = np.argmax(similarities)   # bayes rule seems does not work
+                else:
+                    most_likely_index = np.argmax([entity.probability for entity in possible_entities])
+            possible_entities[most_likely_index].start_pos = sentence[best_config[0]].start_pos
+            possible_entities[most_likely_index].end_pos = sentence[best_config[1]].end_pos
+            most_likely_entities.append(possible_entities[most_likely_index])
+        else:
+            token_index += 1
+    logging.log(logging.DEBUG, most_likely_entities)
+    return most_likely_entities if most_likely_entities else None
+
--- src_python/test_wikipedia_adapter.py
+++ src_python/test_wikipedia_adapter.py
@@ -1,6 +1,10 @@
 from wikidata_adapter import WikidataAdapter
+from wikidata_adapter_v2 import WikidataAdapter_V2
 
 def test_work():
     n = WikidataAdapter('apple')
     assert n.status == 200
 
+def test_work_v2():
+    n = WikidataAdapter_V2('apple')
+    assert n.status == 200
