f0f36b8b11b53c8526c01a55aec25140113b4765
minhtriet <mtriet.chau@gmail.com>
2020-08-13 04:00:04 +0200

    fix space error

--- src_python/wikidata_adapter_v2.py
+++ src_python/wikidata_adapter_v2.py
@@ -19,40 +19,21 @@ class WikidataAdapter_V2(wikidata_adapter.WikidataAdapter):
                    'type': 'item',
                    'origin': '*',}
 
+    def __new__(cls, text):
+        obj = super(WikidataAdapter_V2, cls).__new__(cls)
+        obj.status, obj._json = asyncio.run(WikidataAdapter_V2.get_info(WikidataAdapter_V2.search_dict, {'search': text}))
+        obj._json = obj._json['search']
+        if not obj._json or not obj._json[0]['label'].lower().startswith(text):
+            return
+        return obj
+    
     def __init__(self, entity_name, result_number=MAX_RESULTS_NUMBER):
-        search_dict = {'action': 'query',
-                   'list': 'search',
-                   'srlimit': result_number,
-                   'format': 'json',}
         self.entity = entity_name
-        self.status, self.json = asyncio.run(WikidataAdapter_V2.get_info(search_dict, {'srsearch': self.entity}))
-        self.json = self.json['query']
+
 
     async def _get_probabilities(self):
-        json_probabilities = [WikidataAdapter_V2.get_info(WikidataAdapter_V2.search_dict,
+        json_probabilities = [WikidataAdapter_V2.get_info(WikidataAdapter_V2.claims_pagelink_dict,
                                                        {'gsrsearch': json_entity['title']}) for json_entity in self.json['search']]
         results = await asyncio.gather(*json_probabilities)
         return results
 
-    def to_entity_list(self):
-        """
-
-        Returns
-        -------
-        total claims
-        detailed dict
-        """
-        result = asyncio.run(self._get_probabilities())
-        if result:  # something is found on wikidata
-            statuses, responses = zip(*result)
-            responses_dict = [response['query']['pages'].values() for response in responses]
-            flattened_responses = list(itertools.chain.from_iterable(responses_dict))
-            probabilities_dict = {}
-            for flattened_response in flattened_responses:
-                probabilities_dict[flattened_response['title']] = float(flattened_response['pageprops']['wb-claims']) + float(flattened_response['pageprops']['wb-sitelinks'])
-            denominator = sum([x for x in probabilities_dict.values()])
-            entities = [base_adapter.Entity(result['title'],
-                                            float(probabilities_dict[result['title']]) / denominator,
-                                            result['snippet']) for result in self.json['search'] if result['snippet']]
-            return denominator, entities
-        return 0, None
